2064fe8ed262cd80db6dfba7353df04a469c6ffe
malusanacoza-ui <malusana.co.za@gmail.com>
2026-01-13 09:52:26 +0200

    created a new HTML folder and added a code

--- Stopwatch3D.Api/Controllers/StopwatchController.cs
+++ Stopwatch3D.Api/Controllers/StopwatchController.cs
@@ -1,6 +1,42 @@
-﻿namespace Stopwatch3D.Api.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+[ApiController]
+[Route("api/stopwatch")]
+public class StopwatchController : ControllerBase
 {
-    public class StopwatchController
+    private static Stopwatch stopwatch = new Stopwatch();
+
+    [HttpPost("start")]
+    public IActionResult Start()
     {
+        stopwatch.Start();
+        return Ok("Started");
+    }
+
+    [HttpPost("stop")]
+    public IActionResult Stop()
+    {
+        stopwatch.Stop();
+        return Ok("Stopped");
+    }
+
+    [HttpPost("reset")]
+    public IActionResult Reset()
+    {
+        stopwatch.Reset();
+        return Ok("Reset");
+    }
+
+    [HttpGet("time")]
+    public IActionResult GetTime(double speed)
+    {
+        double distance = speed * stopwatch.Elapsed.TotalHours;
+
+        return Ok(new
+        {
+            time = stopwatch.Elapsed.ToString(@"hh\:mm\:ss"),
+            distance = Math.Round(distance, 2)
+        });
     }
 }
